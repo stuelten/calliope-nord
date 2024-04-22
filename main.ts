30e5534358bd1cf992e5da3f289d8be966bd64ff
Timo Stülten <8837182+stuelten@users.noreply.github.com>
2024-04-22 23:25:02 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
-basic.showIcon(IconNames.Diamond, 600)
+basic.showIcon(IconNames.SmallHeart, 600)
 basic.setLedColors(0xff0000, 0xff0000, 0xff0000, 1)
 basic.showIcon(IconNames.Heart)
 basic.showString("hi!")
-basic.setLedColor(0x00ff00)
 basic.setLedColors(0x00ff00, 0x00ff00, 0x00ff00, 1)
 basic.forever(function () {
-    basic.showCompass(10)
+    basic.showCompass(100)
     if (input.compassHeading() > 350 || input.compassHeading() < 10) {
         basic.setLedColors(0x00ff00, 0x00ff00, 0x00ff00, 1)
     } else {
